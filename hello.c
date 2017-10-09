c242d02ddefb6860a402ef0c0fa3d3a81e6f8afb
Mohd Samgan Khan <mohdsamgankhan@gmail.com>
2017-10-10 00:50:25 +0530

    hello world

---
+++ hello.c
@@ -0,0 +1,18 @@
+/*
+ * Name: Hello world
+ * Author: samgan
+ * Create Date: 10th October, 2017
+ * Last Modified: 10th October, 2017
+ * Description: a hello world program for introduction of C. 
+ */
+
+#include <stdio.h> //including standerd input output
+
+/**
+ * main function: entery point of a C program
+ * @return void return void means this function will not return anything.
+ */
+void main() 
+{
+	printf("Hello World\n");
+}
